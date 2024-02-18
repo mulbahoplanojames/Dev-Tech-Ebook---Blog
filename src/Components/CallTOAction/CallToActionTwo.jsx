dd30fbda9a9d7add95df1457567347dfacabe7c5
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-02-18 11:48:21 +0200

    Completed the Footer Component and the InstagramCallToAction Component, Made some modification with in the index.css file

--- src/Components/CallTOAction/CallToActionTwo.jsx
+++ src/Components/CallTOAction/CallToActionTwo.jsx
@@ -1,4 +1,5 @@
 import { FaInstagram } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const CallToActionTwo = () => {
   return (
@@ -6,13 +7,15 @@ const CallToActionTwo = () => {
       <div className="py-12 lg:px-36 px-4">
         <div className="call-to-action bg-gradient-to-r from-pink-500 via-red-400 to-red-500 ">
           <h1 className="lg:text-3xl text-2xl text-white">
-            {" "}
             Join Me on Telegram for more free contents
           </h1>
           <div className="lg:w-28 lg:h-28 w-24 h-16  bg-red-800 rounded-full flex justify-center items-center">
-            <a href="" target="blank">
-              <FaInstagram className="lg:size-20 size-8 text-white" />
-            </a>
+            <Link
+              to="https://www.instagram.com/oplanojames?igsh=ZnRkc2MwOXlqYTdu"
+              target="blank"
+            >
+              <FaInstagram className="lg:size-20 size-8 text-white" />``
+            </Link>
           </div>
         </div>
       </div>
--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,9 +1,68 @@
-import { FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
+import {
+  FaInstagram,
+  FaFacebook,
+  FaTwitter,
+  FaWhatsapp,
+  FaHome,
+  FaBook,
+} from "react-icons/fa";
+
+import { BsCardList } from "react-icons/bs";
+import { IoIosPeople } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
-      <div className="">Footer</div>
+      <div className="lg:px-20 px-3 py-12 bg-1">
+        <div className="grid lg:grid-cols-3 grid-cols-1 gap-12 place-items-center text-white">
+          <h1 className="text-6xl text-white font-semibold">Dev!tech</h1>
+          <div className="order-first">
+            <h1 className="text-2xl pb-2 ">Info</h1>
+            <p className="pb-2 flex items-center gap-x-2 text-lg text-white">
+              <FaHome className="" />
+              <Link to="">Home</Link>
+            </p>
+            <p className="pb-2 flex items-center gap-x-2 text-lg ">
+              <FaBook />
+              <Link to="">E-books</Link>
+            </p>
+            <p className="pb-2 flex items-center gap-x-2 text-lg ">
+              <BsCardList />
+              <Link to="">Blogs</Link>
+            </p>
+            <p className="pb-2 flex items-center gap-x-2 text-lg ">
+              <IoIosPeople />
+              <Link to="">About Us</Link>
+            </p>
+          </div>
+          <div className="">
+            <h1 className="text-2xl pb-4">Follow us for more</h1>
+            <div className="flex gap-7">
+              <div className="icon-wripper bg-red-300">
+                <Link to="">
+                  <FaInstagram className="size-7" />
+                </Link>
+              </div>
+              <div className="icon-wripper bg-blue-300">
+                <Link to="">
+                  <FaFacebook className="size-7" />
+                </Link>
+              </div>
+              <div className="icon-wripper bg-green-300">
+                <Link to="">
+                  <FaWhatsapp className="size-7" />
+                </Link>
+              </div>
+              <div className="icon-wripper bg-black">
+                <Link to="">
+                  <FaTwitter className="size-7" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
