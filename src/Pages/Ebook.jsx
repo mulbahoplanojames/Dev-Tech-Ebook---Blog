10cf04dc7c52cdb51e13442a3f36d0bd0a85e729
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-02-25 13:24:24 +0200

    remove the alert from the ebook and blogs component for a better user experience

--- src/Pages/Ebook.jsx
+++ src/Pages/Ebook.jsx
@@ -1,17 +1,7 @@
-import Swal from "sweetalert2";
 import TelegramCallToAction from "../Components/CallTOAction/CallToAction";
 import Ebooks from "../Components/Ebooks/Ebooks";
-import { useEffect } from "react";
 
 const Ebook = () => {
-  useEffect(() => {
-    Swal.fire({
-      title: "Welcome to our Ebooks",
-      text: "Feel free to reach out through the contact from. Your feedback, questions and suggestions are important to us",
-      icon: "info",
-    });
-  }, []);
-
   return (
     <>
       <Ebooks />
