4573ad9f8851bae1fcfd4126a1536e39b3c305c4
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-03-24 18:14:22 +0200

    Added Aminate.css to the project for a smooth and easy aminations

--- src/Components/Our-Offers/Offers.jsx
+++ src/Components/Our-Offers/Offers.jsx
@@ -3,28 +3,28 @@ import Ebook from "/src/assets/ebook.png";
 import Blog from "/src/assets/blog.png";
 
 const Offers = () => {
-  return (
-    <>
-      <div className="py-16 lg:px-20 px-3 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 place-items-center gap-8">
-        <OffersCard
-          title="Ebook"
-          description={
-            " Explore our carefully curated collection of well-structured and simplified ebooks, designed to make reading a seamless and enjoyable experience."
-          }
-          image={Ebook}
-          href={"/ebooks"}
-        />
-        <OffersCard
-          title="Blogs"
-          description={
-            " Explore our carefully curated collection of well-structured and simplified blog posts, designed to make reading a seamless and enjoyable experience."
-          }
-          image={Blog}
-          href={"/blog"}
-        />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='py-16 lg:px-20 px-3 grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 place-items-center gap-8 animate__animated  animate__fadeInUpBig'>
+				<OffersCard
+					title='Ebook'
+					description={
+						" Explore our carefully curated collection of well-structured and simplified ebooks, designed to make reading a seamless and enjoyable experience."
+					}
+					image={Ebook}
+					href={"/ebooks"}
+				/>
+				<OffersCard
+					title='Blogs'
+					description={
+						" Explore our carefully curated collection of well-structured and simplified blog posts, designed to make reading a seamless and enjoyable experience."
+					}
+					image={Blog}
+					href={"/blog"}
+				/>
+			</div>
+		</>
+	);
 };
 
 export default Offers;
