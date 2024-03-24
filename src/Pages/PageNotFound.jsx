4ae2a28ce7a1241a97705597c402613cfd1e2b23
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-03-24 17:59:09 +0200

    Added the The pageNot found Page to the site

---
+++ src/Pages/PageNotFound.jsx
@@ -0,0 +1,18 @@
+const PageNotFound = () => {
+	return (
+		<>
+			<div className='mt-36 mb-32 text-center'>
+				<h1 className='text-8xl'>404</h1>
+				<p className='pt-6'>
+					The page you are trying to accces is not Avaliable or the URL has
+					being change
+				</p>
+				<p className='pt-2'>
+					Please us the navbar that will take yo to a specific page on this site
+				</p>
+			</div>
+		</>
+	);
+};
+
+export default PageNotFound;
