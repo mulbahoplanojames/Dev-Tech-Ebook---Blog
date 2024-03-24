d19e15f42813cebf584dd884337e681d566101cb
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-03-24 18:16:43 +0200

    Added Aminate.css to the project for a smooth and easy aminations

--- src/Components/OurEbook/OurEbooks.jsx
+++ src/Components/OurEbook/OurEbooks.jsx
@@ -5,28 +5,28 @@ import Button from "../../Interfaces/Button";
 import { Link } from "react-router-dom";
 
 const OurEbooks = () => {
-  return (
-    <>
-      <div className=" py-20 lg:px-40 px-3 text-center">
-        <h1 className="text-4xl  pb-16">Our Ebooks</h1>
-        <div className="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10 place-items-center mb-16">
-          {ourEbooks.map((ebook) => {
-            return (
-              <EbookCard
-                key={ebook.id}
-                title={ebook.title}
-                image={ebook.image}
-                href={ebook.href}
-              />
-            );
-          })}
-        </div>
-        <Link to="/ebooks">
-          <Button text={"Click here for more e-books"} className="mt-12" />
-        </Link>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className=' py-20 lg:px-40 px-3 text-center'>
+				<h1 className='text-4xl  pb-28'>Our Ebooks</h1>
+				<div className='grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10 place-items-center mb-16 animate__animated animate__fadeIn'>
+					{ourEbooks.map((ebook) => {
+						return (
+							<EbookCard
+								key={ebook.id}
+								title={ebook.title}
+								image={ebook.image}
+								href={ebook.href}
+							/>
+						);
+					})}
+				</div>
+				<Link to='/ebooks'>
+					<Button text={"Click here for more e-books"} className='mt-12' />
+				</Link>
+			</div>
+		</>
+	);
 };
 
 export default OurEbooks;
